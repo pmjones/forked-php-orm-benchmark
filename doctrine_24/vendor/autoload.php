2fbd4a111cbfd6b2638e417991166e8edb4f4fdf
Michal Piotrowski <michal@eventhorizon.pl>
2014-04-17 23:58:20 +0200

    doctrine 2.4 update

--- doctrine_24/vendor/autoload.php
+++ doctrine_24/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit0c01c59e9fe7ca81524bcfa76c355f34::getLoader();
+return ComposerAutoloaderInit1769942080c17450b0a754c5e4a28abf::getLoader();
