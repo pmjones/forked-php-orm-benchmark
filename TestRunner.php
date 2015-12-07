641f1e8156e158283eda3079b8c7b5b934998331
Fredrik Wollsén <fredrik@neam.se>
2015-12-07 17:09:18 +0200

    Not running broken test suites

--- TestRunner.php
+++ TestRunner.php
@@ -12,6 +12,12 @@ passthru('php yii1_m/TestRunner.php');
 echo $separator;
 passthru('php yii2_m/TestRunner.php');
 echo $separator;
+// Fuel is broken - "No composer autoloader found. Please run composer to install the FuelPHP framework dependencies first!"
+//passthru('php fuel_173/TestRunner.php');
+//echo $separator;
+// phpixie is broken - "Fatal error: Uncaught exception 'PHPixie\ORM\Exception\Plan' with message 'Result used as update source must contain a single item.' in /repo/phpixie/vendor/phpixie/orm/src/PHPixie/ORM/Steps/Step/Update/Map.php:21"
+#passthru('php phpixie/TestRunner.php');
+#echo $separator;
 //passthru('php propel_14/TestRunner.php');
 //echo $separator;
 //passthru('php propel_15/TestRunner.php');
@@ -27,8 +33,9 @@ echo $separator;
 //passthru('php propel_17_with_cache/TestRunner.php');
 passthru('php propel_20/TestRunner.php');
 echo $separator;
-passthru('php propel_dm/TestRunner.php');
-echo $separator;
+// Propel dm is broken - "Fatal error: Class undefined: Propel\Runtime\Configuration in /repo/propel_dm/generated-conf/config.php on line 4"
+//passthru('php propel_dm/TestRunner.php');
+//echo $separator;
 //passthru('php doctrine_12/TestRunner.php');
 //echo $separator;
 //passthru('php doctrine_2/TestRunner.php');
