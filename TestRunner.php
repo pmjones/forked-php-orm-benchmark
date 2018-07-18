986add5e094ae906d0eeb4d7ac491e30c1002543
Paul M. Jones <pmjones88@gmail.com>
2018-07-18 07:44:31 -0500

    disable apples/oranges and breakages

--- TestRunner.php
+++ TestRunner.php
@@ -7,56 +7,78 @@ $separator = "|                                  |        |        |        |   
 
 passthru('php raw_pdo/TestRunner.php');
 echo $separator;
-// passthru('php lessql/TestRunner.php');
-// echo $separator;
+
+passthru('php lessql/TestRunner.php');
+echo $separator;
+
 // passthru('php yii1_m/TestRunner.php');
 // echo $separator;
+
+## Cannot use 'Object' as class name as it is reserved in /yii2_m/vendor/yiisoft/yii2/base/Object.php on line 77
 // passthru('php yii2_m/TestRunner.php');
 // echo $separator;
-// Fuel is broken - "No composer autoloader found. Please run composer to install the FuelPHP framework dependencies first!"
-//passthru('php fuel_173/TestRunner.php');
-//echo $separator;
-// phpixie is broken - "Fatal error: Uncaught exception 'PHPixie\ORM\Exception\Plan' with message 'Result used as update source must contain a single item.' in /repo/phpixie/vendor/phpixie/orm/src/PHPixie/ORM/Steps/Step/Update/Map.php:21"
-#passthru('php phpixie/TestRunner.php');
-#echo $separator;
-//passthru('php propel_14/TestRunner.php');
-//echo $separator;
-//passthru('php propel_15/TestRunner.php');
-//echo $separator;
-//passthru('php propel_15_with_cache/TestRunner.php');
-//echo $separator;
-//passthru('php propel_16/TestRunner.php');
-//echo $separator;
-//passthru('php propel_16_with_cache/TestRunner.php');
-//echo $separator;
-//passthru('php propel_17/TestRunner.php');
-//echo $separator;
-//passthru('php propel_17_with_cache/TestRunner.php');
+
+## Uncaught Error: Call to undefined method Error::shutdown_handler() in /fuel_173/fuel/core/bootstrap.php:77
+// passthru('php fuel_173/TestRunner.php');
+// echo $separator;
+
+## Fatal error: Uncaught exception 'PHPixie\ORM\Exception\Plan' with message 'Result used as update source must contain a single item.' in /phpixie/orm/src/PHPixie/ORM/Steps/Step/Update/Map.php:21"
+// passthru('php phpixie/TestRunner.php');
+// echo $separator;
+
+// passthru('php propel_14/TestRunner.php');
+// echo $separator;
+
+// passthru('php propel_15/TestRunner.php');
+// passthru('php propel_15_with_cache/TestRunner.php');
+// echo $separator;
+
+// passthru('php propel_16/TestRunner.php');
+// passthru('php propel_16_with_cache/TestRunner.php');
+// echo $separator;
+
+// passthru('php propel_17/TestRunner.php');
+// passthru('php propel_17_with_cache/TestRunner.php');
+// echo $separator;
+
+## Fatal error:  Declaration of Propel\Runtime\Collection\OnDemandCollection::offsetGet($offset) must be compatible with & Propel\Runtime\Collection\Collection::offsetGet($offset) in /propel_20/vendor/propel/propel/src/Propel/Runtime/Collection/OnDemandCollection.php on line 216
 // passthru('php propel_20/TestRunner.php');
 // echo $separator;
+
 // Propel dm is broken - "Fatal error: Class undefined: Propel\Runtime\Configuration in /repo/propel_dm/generated-conf/config.php on line 4"
 //passthru('php propel_dm/TestRunner.php');
 //echo $separator;
-//passthru('php doctrine_12/TestRunner.php');
-//echo $separator;
-//passthru('php doctrine_2/TestRunner.php');
-//echo $separator;
-//passthru('php doctrine_21/TestRunner.php');
-//echo $separator;
-//passthru('php doctrine_22/TestRunner.php');
-//echo $separator;
-//passthru('php doctrine_23/TestRunner.php');
-//echo $separator;
-//passthru('php doctrine_24/TestRunner.php');
-//echo $separator;
+
+// passthru('php doctrine_12/TestRunner.php');
+// echo $separator;
+
+// passthru('php doctrine_2/TestRunner.php');
+// echo $separator;
+
+// passthru('php doctrine_21/TestRunner.php');
+// echo $separator;
+
+// passthru('php doctrine_22/TestRunner.php');
+// echo $separator;
+
+// passthru('php doctrine_23/TestRunner.php');
+// echo $separator;
+
+// passthru('php doctrine_24/TestRunner.php');
+// echo $separator;
+
 passthru('php doctrine_26/TestRunner.php');
 echo $separator;
+
+// passthru('php atlas_2/TestRunner.php');
+// echo $separator;
+
 passthru('php atlas_3/TestRunner.php');
 echo $separator;
-// if (version_compare(PHP_VERSION, '5.5.9', '>=')) {
-// 	passthru('php eloquent/TestRunner.php');
-// 	echo $separator;
-// }
+
+passthru('php eloquent/TestRunner.php');
+echo $separator;
+
 // passthru('php phalcon/TestRunner.php');
 // echo $separator;
 echo "|-------------------------------------------------------------------------------------------------------|\n";
