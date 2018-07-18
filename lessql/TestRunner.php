3238877814251c99c591ccb070cc7322bb0d3a44
Paul M. Jones <pmjones88@gmail.com>
2018-07-18 07:43:40 -0500

    turn off deprecation errors for LesSQL

--- lessql/TestRunner.php
+++ lessql/TestRunner.php
@@ -6,5 +6,6 @@ $time = microtime(true);
 $memory = memory_get_usage();
 $test = new LessQLTestSuite();
 $test->initialize();
+error_reporting(0);
 $test->run();
 echo sprintf(" %11s | %6.2f |\n", number_format(memory_get_usage(true) - $memory), (microtime(true) - $time));
